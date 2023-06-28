6392c8bc157502c040c21bce40f821f256b2410f
Manish Singh <astromanishsingh@gmail.com>
2023-06-28 12:00:58 +0530

    personality to one bot added

--- config.py
+++ config.py
@@ -1,23 +1,27 @@
-OPENAI_API_KEY = "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"
 PERSONALITY = [
-    'expert in generating python function', 
+    'expert in generating python function',
+    'proficient in data analysis using pandas',
+    'skilled in numerical computing with numPy'
 ]
 KNOWLEDGE_DOMAIN = [
     'python', 'pandas', 'numPy'
 ]
+
+COMMANDS = {
+    '/help': 'list five things you can do best in a concise manner',
+    '/examples': 'provide code examples for different Python functions',
+    '/data': 'perform data analysis and manipulation using pandas',
+    '/math': 'demonstrate numerical computations using numPy',
+    '/project': 'assist in developing a Python project from scratch'
+}
+
 BOTS = [
     {
-        'id':0,
-        'personality': PERSONALITY[0],
-        'conversation':[
-            {
-                'role':'system',
-                'content': f'''you are {PERSONALITY[0]}. you know about {KNOWLEDGE_DOMAIN[0]}. you generate python code only. you use production ready functions and variables name. you write modular and manageable code.'''
-            },
-            {
-                'role':'user',
-                'content':'connect to aws s3'
-            }
-        ],
+        'id': 0,
+        'personality': f'''You are {PERSONALITY[0]}.
+            You have expertise in {KNOWLEDGE_DOMAIN[0]} and are proficient in {KNOWLEDGE_DOMAIN[1]} and {KNOWLEDGE_DOMAIN[2]}.
+            You respond to the following commands: {COMMANDS}.
+            You specialize in generating Python code, using production-ready functions and variables.
+            You prioritize writing modular and manageable code.'''
     }
 ]
--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import openai
 
-def generate_response(messages):
+def generate_response(conversation):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0613",
-        messages=messages,
-        max_tokens=500,
+        messages=conversation,
+        max_tokens=100,
         temperature=0,
     )
     return response
